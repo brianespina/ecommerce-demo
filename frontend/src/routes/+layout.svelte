60126b3b2b9b862dfcf49d9ecb4f94ca1a83513f
Brian Espina <espinabrian@gmail.com>
2025-10-22 12:21:40 +0800

    protected routes setup

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import SuperTokens from 'supertokens-web-js';
+	import Session from 'supertokens-web-js/recipe/session';
+	import EmailPassword from 'supertokens-web-js/recipe/emailpassword';
+
+	if (typeof window !== 'undefined') {
+		SuperTokens.init({
+			appInfo: {
+				apiDomain: 'http://localhost:8080/',
+				apiBasePath: '/auth',
+				appName: '...'
+			},
+			recipeList: [Session.init(), EmailPassword.init()]
+		});
+	}
 
 	let { children } = $props();
 </script>
---
+++ frontend/src/routes/protected/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Session from 'supertokens-web-js/recipe/session';
+
+	let isLogged = $state(false);
+	let isLoading = $state(true);
+
+	async function doesSessionExist() {
+		if (typeof window === 'undefined') return {};
+
+		if (await Session.doesSessionExist()) {
+			// user is logged in
+			isLogged = true;
+		} else {
+			// user has not logged in yet
+			isLogged = false;
+		}
+		isLoading = false;
+	}
+	doesSessionExist();
+</script>
+
+{#if isLoading}
+	...Loading
+{:else if isLogged}
+	Welcome user
+{:else}
+	Welcome guest
+{/if}
